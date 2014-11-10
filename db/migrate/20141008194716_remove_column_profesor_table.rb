35669514489d2be7fa62707a52f1e2633789c74e
JarrodTarico <jarrod.parks93@gmail.com>
2014-11-10 21:18:30 +0000

    Migrations deleted

--- db/migrate/20141008194716_remove_column_profesor_table.rb
+++
@@ -1,4 +0,0 @@
-class RemoveColumnProfesorTable < ActiveRecord::Migration
-  def change
-  end
-end
--- db/migrate/20141008195113_add_name_to_professor.rb
+++
@@ -1,6 +0,0 @@
-class AddNameToProfessor < ActiveRecord::Migration
-  def change
-    add_column :professors, :First_Name, :string
-    add_column :professors, :Last_Name, :string
-  end
-end
