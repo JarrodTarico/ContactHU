5a7fc2ca49d984f1984005bc775bcee46dc03332
JarrodTarico <jarrod.parks93@gmail.com>
2014-11-19 17:34:30 +0000

    More Changes to DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141117194255) do
+ActiveRecord::Schema.define(version: 20141119172459) do
 
   create_table "course_informations", force: true do |t|
     t.string   "Course_Title"
